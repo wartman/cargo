23a0996e4be8b49e4069a64dbf3392d33289905f
Peter Wartman <pwartman@wartfolio.com>
2014-11-02 13:31:47 -0600

    Better api start

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 require('dotenv').load()
+var path = require('path')
 var Rabbit = require('./lib/server/rabbit')
 
 Rabbit({
-  baseDir: __dirname + '/',
-  themesDir: 'content/theme',
+
+  'host': 'localhost',
+  'port': '8080',
+
+	'views': path.join(__dirname, './content/theme/'),
+
+	'database': {
+		development: {
+	    client: 'sqlite3',
+	    connection: {
+	      filename: path.join(__dirname, '/content/data/rabbit-dev.db')
+	    },
+	    debug: false
+	  },
+	  production: {
+	    client: 'sqlite3',
+	    connection: {
+	      filename: path.join(__dirname, '/content/data/rabbit.db')
+	    },
+	    debug: false
+	  }
+  }
+
 }).run()
