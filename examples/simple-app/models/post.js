d3e2b3603fcafe33b6b68f97360fc850149b4561
CSA <faxmaster@gmail.com>
2015-01-07 17:33:15 -0600

    Update

--- examples/simple-app/models/post.js
+++ examples/simple-app/models/post.js
@@ -1,12 +1,10 @@
 var rabbit = require('../../../')
 var fields = rabbit.db.fields
 
-var Post = new rabbit.db.Schema('Post', {
+rabbit.db.Schema.add('Post', {
   'id': fields.primaryKey(),
   'title': fields.string({nullable: false}),
   'content': fields.text(),
   'created': fields.dateTime({nullable:false}),
   'updated': fields.dateTime()
 })
-
-Post.register()
--- test/db/setup/models/test.js
+++ test/db/setup/models/test.js
@@ -2,19 +2,20 @@ var rabbit = require('../../../../')
 var fields = rabbit.db.fields
 
 // A test model
-var Test = new rabbit.db.Schema('Test', {
+rabbit.db.Schema('Test', {
   'id': fields.primaryKey(),
   'name': fields.string({nullable: false}),
   'content': fields.text(),
   'number': fields.integer(),
   'created': fields.dateTime({nullable:false}),
   'updated': fields.dateTime()
-})
 
-Test.addMethods({
-  sayFoo: function () {
-    return 'foo'
+  methods: {
+    sayFoo: function () {
+      return 'foo'
+    }
   }
+  
 })
 
 Test.register()
