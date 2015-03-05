9b14d66dbae9e2c1dc76b5e8090480491fb0f7a2
CSA <faxmaster@gmail.com>
2015-03-05 14:14:11 -0600

    removed unused code and deps

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,6 @@ var fs = require('fs')
 var path = require('path')
 var gulp = require('gulp')
 var mocha = require('gulp-mocha')
-var stylus = require('gulp-stylus')
-var clean = require('gulp-clean')
-var nib = require('nib')
-var merge = require('merge-stream')
-
-// Helpers
-// -------
-
-// Get folders in the specified dir.
-function getFolders(dir) {
-  return fs.readdirSync(dir)
-    .filter(function (file) {
-      // Make sure the file is a directory
-      return fs.statSync(path.join(dir, file)).isDirectory()
-    })
-}
 
 // Tasks
 // -----
@@ -36,20 +20,4 @@ gulp.task('test (integration)', ['test (unit)'], function () {
     .pipe(mocha({reporter: 'spec'}))
 })
 
-// Build styles for each theme
-// gulp.task('build-styles', function () {
-//   var folders = getFolders('content/theme')
-//   var tasks = folders.map(function (folder) {
-//     var folderPath = path.join('content/theme', folder)
-//     return gulp.src(path.join(folderPath, 'assets/css/stylus/main.styl'))
-//       .pipe(stylus({
-//         use: nib(),
-//         compress: false
-//       }))
-//       .pipe(gulp.dest(path.join(folderPath, 'assets/css/build')))
-//   })
-//   return merge(tasks)
-// })
-
 gulp.task('default', ['test (unit)', 'test (integration)'])
-// gulp.task('build', ['build-styles'])
